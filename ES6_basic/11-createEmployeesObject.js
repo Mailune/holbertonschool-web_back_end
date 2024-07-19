0edbc3ea9fa273129035ba17792c95094113d236
Mailune <mailysrosereau@yahoo.fr>
2024-07-19 12:20:31 +0200

    fix with lint

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const obj = {
-      [departmentName]: employees,
-    };
-  
-    return obj;
-  }
+  const obj = {
+    [departmentName]: employees,
+  };
+
+  return obj;
+}
