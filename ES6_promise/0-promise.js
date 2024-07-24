4e2d2701590b480be00e84048e9dce988e5d7f8d
Mailune <mailysrosereau@yahoo.fr>
2024-07-25 00:44:17 +0200

    remove reject

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,8 @@
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     const success = true; // Simulate API call
     if (success) {
-      resolve("API response received successfully");
-    } else {
-      reject(new Error("Failed to receive API response"));
+      resolve('API response received successfully');
     }
   });
 }
