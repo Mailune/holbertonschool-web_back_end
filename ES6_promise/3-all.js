6acff79383ecd373141b96560826fafa2d78f9d8
Mailune <mailysrosereau@yahoo.fr>
2024-07-25 00:55:02 +0200

    Refactor handleProfileSignup to use destructuring for clarity

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,13 +1,12 @@
 import { uploadPhoto, createUser } from './utils.js';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const { body } = values[0];
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+      console.log(`${body} ${firstName} ${lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
