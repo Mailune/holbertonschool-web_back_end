cd17bdc8fa519e4bcad4b05e364dfa1c659cc311
Mailune <mailysrosereau@yahoo.fr>
2024-08-15 22:38:24 +0200

    first commit tasks

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+"""Function that takes an integer max_delay and returns an asyncio.Task"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Function that takes an integer max_delay and returns an asyncio.Task.
+
+    Args:
+    max_delay (int): Maximum delay value to be passed to the wait_random coroutine.
+
+    Returns:
+    asyncio.Task: An asyncio.Task object that schedules the wait_random coroutine to be run concurrently.
+    """
+    return asyncio.create_task(wait_random(max_delay))
