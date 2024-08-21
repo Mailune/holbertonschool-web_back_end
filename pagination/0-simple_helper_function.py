7eee87a2ba94acf5797753b50423b05b490e2bfe
Mailune <mailysrosereau@yahoo.fr>
2024-08-22 00:07:54 +0200

    fix pycodestyle lines too long

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -9,7 +9,8 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    Calculate the start and end index for the range of items to return in a list
+    Calculate the start and end index for the range
+    of items to return in a list
     for pagination purposes.
 
     Args:
@@ -17,7 +18,8 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
         page_size (int): The number of items per page.
 
     Returns:
-        Tuple[int, int]: A tuple containing the start index (inclusive) and the end index (exclusive).
+        Tuple[int, int]: A tuple containing the start index (inclusive)
+        and the end index (exclusive).
     """
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
